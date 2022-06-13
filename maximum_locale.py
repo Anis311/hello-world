17c649700d3b3d58c8211b662171f4586ec539dd
Anis <a.laamri@outlook.fr>
2022-06-13 15:31:10 +0200

    description of the commit

---
+++ maximum_locale.py
@@ -0,0 +1,20 @@
+"""
+Small example of recursion 2
+"""
+
+def find_maximum(table):
+    """
+    This function finds the local minimum in a list of integer T
+
+    Assumptions:
+    - T[0] < T[1] and T[n-2] > T[n-1]
+    - all values are different
+    """
+
+    #Implemtation goes here!
+    return None
+
+
+MY_TABLE = [3, 5, 4, 8, 2, 7, 22, 1, 11, 10]
+
+print("The maximum value is ", find_minimum(MY_TABLE))
